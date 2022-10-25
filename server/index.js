e28ae4bc816fd1976ac260c6839fb47b62f34bc6
ShivanshAr97 <96040322+ShivanshAr97@users.noreply.github.com>
2022-10-26 01:17:55 +0530

    fifth commit

--- server/index.js
+++ server/index.js
@@ -1,48 +1,15 @@
-// import fetch  from "node-fetch";
+// import { CourierClient } from "@trycourier/courier";
+const CourierClient = require('@trycourier/courier')
 
-// const options = {
-//   method: 'POST',
-//   headers: {
-//     Accept: 'application/json',
-//     'Content-Type': 'application/json',
-//     Authorization: 'Bearer pk_test_X1PVT7H5N0MCKHGNVBQXWMTH68VQ'
-//   },
-//   body: JSON.stringify({
-//     "message": {
-//         "to": {
-//             "email": "shivansh.arora973@gmail.com"
-//         },
-//         "template": "abcdef12345678",
-//         "data": {
-//             "name": "John Doe"
-//         },
-//         "routing": {
-//             "method": "single",
-//             "channels": [
-//                 "email"
-//             ]
-//         }
-//     }
-// })
-// };
+const courier = CourierClient({ authorizationToken: "pk_test_X1PVT7H5N0MCKHGNVBQXWMTH68VQ" });
 
-// fetch('https://api.courier.com/send', options)
-//   .then(response => response.json())
-//   .then(response => console.log(response))
-//   .catch(err => console.error(err));
-
-import { CourierClient } from "@trycourier/courier";
-
-const courier = CourierClient({ authorizationToken: "pk_test_X1PVT7H5N0MCKHGNVBQXWMTH68VQ" }); // get from the Courier UI
-
-// Example: send a basic message to an email recipient
 const { requestId } = await courier.send({
   message: {
     to: {
       data: {
-        name: "Aditi Arora",
+        name: "Shivansh Arora",
       },
-      email: "5aditi.arora94@gmail.com",
+      email: "shivansh.arora973@gmail.com",
     },
     template: "7G65GYEYFCMHTGHT1483FCMQ144Z",
     routing: {
--- src/nav_components/Email.js
+++ src/nav_components/Email.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import Starter from '../images/Starter.png'
 import { Link } from "react-router-dom"
+import { useState } from 'react'
 
 const Email = () => {
+    const [sub, setSub] = useState('Subscribe')
+
+    function Subscribed() {
+        setSub(`Done :)`)
+    }
+
   return (
     <>
       <div id='mail' className='flex justify-center bg-[#121212] p-8'>
@@ -15,7 +22,7 @@ const Email = () => {
             <p className='text-sm py-6 text-gray-500 text-center font-semibold'>Make health & fitness your top priority with tips on workouts, healthy eating, wellness and more, straight from the experts.</p>
             <div className='text-center'>
               <input className='border text-sm py-3 px-7 bg-num2 duration-300 rounded-lg hover:bg-white' type="text" name="" id="" placeholder='Email Address' />
-              <button className="btn text-white font-bold rounded-lg bg-num1 px-5 py-3 mx-4 text-sm hover:bg-red-500">Subscribe</button>
+              <button onClick={Subscribed} className="btn text-white font-bold rounded-lg bg-num1 px-5 py-3 mx-4 text-sm hover:bg-red-500">{sub}</button>
             </div>
           </div>
         </div>
